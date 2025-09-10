a046b70fb5c2d9b001ae1fc15c8dadebc6a1dde5
Zeyad Hany <zezohany2512@gmail.com>
2025-09-10 22:04:33 +0300

    update(faied/success): render the success and failed models on payment

--- src/components/modals.tsx
+++ src/components/modals.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { SubscriptionModal } from "@/features/subscription/components/subscription-modal";
+import { FailModal } from "@/features/subscription/subscriptions/components/fail-modal";
+import { SuccessModal } from "@/features/subscription/subscriptions/components/success-modal";
 import { useEffect, useState } from "react";
 
 export const Modals = () => {
@@ -15,6 +17,8 @@ export const Modals = () => {
 
 	return (
 		<>
+			<FailModal />
+			<SuccessModal />
 			<SubscriptionModal />
 		</>
 	);
--- src/features/subscription/subscriptions/components/subscription-alert.tsx
+++ src/features/subscription/subscriptions/components/subscription-alert.tsx
@@ -3,27 +3,27 @@
 import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
-import { useFailModal } from "@/features/subscriptions/store/use-fail-modal";
-import { useSuccessModal } from "@/features/subscriptions/store/use-success-modal";
+import { useFailModal } from "../store/use-fail-modal";
+import { useSuccessModal } from "../store/use-success-modal";
 
 export const SubscriptionAlert = () => {
-  const params = useSearchParams();
+	const params = useSearchParams();
 
-  const { onOpen: onOpenFail } = useFailModal();
-  const { onOpen: onOpenSuccess } = useSuccessModal();
+	const { onOpen: onOpenFail } = useFailModal();
+	const { onOpen: onOpenSuccess } = useSuccessModal();
 
-  const canceled = params.get("canceled");
-  const success = params.get("success");
+	const canceled = params.get("canceled");
+	const success = params.get("success");
 
-  useEffect(() => {
-    if (canceled) {
-      onOpenFail();
-    }
+	useEffect(() => {
+		if (canceled) {
+			onOpenFail();
+		}
 
-    if (success) {
-      onOpenSuccess();
-    }
-  }, [canceled, onOpenFail, success, onOpenSuccess]);
+		if (success) {
+			onOpenSuccess();
+		}
+	}, [canceled, onOpenFail, success, onOpenSuccess]);
 
-  return null;
+	return null;
 };
