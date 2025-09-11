f3ed7ee4dca55460bcd9d904e36a6192d6b15f91
Zeyad Hany <zezohany2512@gmail.com>
2025-09-11 14:56:52 +0300

    add(subscription-alert): to display the subscription feedback when payment

--- src/components/modals.tsx
+++ src/components/modals.tsx
@@ -3,6 +3,7 @@
 import { SubscriptionModal } from "@/features/subscription/components/subscription-modal";
 import { FailModal } from "@/features/subscription/components/fail-modal";
 import { SuccessModal } from "@/features/subscription/components/success-modal";
+import { SubscriptionAlert } from "@/features/subscription/components/subscription-alert";
 import { useEffect, useState } from "react";
 
 export const Modals = () => {
@@ -17,6 +18,7 @@ export const Modals = () => {
 
 	return (
 		<>
+			<SubscriptionAlert />
 			<FailModal />
 			<SuccessModal />
 			<SubscriptionModal />
